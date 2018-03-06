10760caf0d4d5d2d73e109b1bf493e59085a655e
Unknown <vinc.claveau@gmail.com>
2018-03-06 16:48:04 +0100

    [IMP] visite viewAction

--- src/Votop/VirtualVisitBundle/Controller/VisitController.php
+++ src/Votop/VirtualVisitBundle/Controller/VisitController.php
@@ -3,17 +3,26 @@
 namespace Votop\VirtualVisitBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class VisitController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('VotopVirtualVisitBundle:Visit:visit.html.twig');
+        return $this->render('VotopVirtualVisitBundle:Visit:index.html.twig');
     }
-	
-	public function viewAction($id)
-	{
-		return new Response("Visit ID: ".$id);
-	}
+    
+    public function viewAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $visit = $em->getRepository('VotopVirtualVisitBundle:Visit')->find($id);
+        
+        if (null === $visit) {
+            throw new NotFoundHttpException("Visit with ID ".$id." doesn't exist!");
+        }
+    
+        return $this->render('VotopVirtualVisitBundle:Visit:view.html.twig', array(
+            'visit' => $visit
+        ));
+    }
 }
