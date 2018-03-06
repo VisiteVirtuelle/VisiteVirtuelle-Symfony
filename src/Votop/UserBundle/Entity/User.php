37a40a2772f14c580d8e95cd0960a01e84e673a3
Unknown <guillaume.vidal@lycee-bourdelle.fr>
2018-03-06 17:47:17 +0100

    [ADD] Translations
    + email reset mot de passe
    + Ajout champ 'name' dans l'entité User
    + Modification des vues User

--- src/Votop/UserBundle/Entity/User.php
+++ src/Votop/UserBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace Votop\UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as FosUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert; 
 
 /**
  * @ORM\Entity
@@ -18,10 +19,41 @@ class User extends FosUser
 	 */
     protected $id;
 	
+    /** 
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Assert\Length( 
+     *     min=3, 
+     *     max=255, 
+     *     minMessage="The name is too short.", 
+     *     maxMessage="The name is too long.", 
+     *     groups={"Registration", "Profile"} 
+     * ) 
+     */ 
+    protected $name; 
+    
 	public function __construct()
 	{
 		parent::__construct();
 	}
- }
+    
+    /** 
+     * {@inheritdoc} 
+     */ 
+    public function getName() 
+    { 
+        return $this->name; 
+    } 
+   
+    /** 
+     * {@inheritdoc} 
+     */ 
+    public function setName($name) 
+    { 
+        $this->name = $name; 
  
- ?>
+        return $this; 
+    }
+}
+ 
+?>
