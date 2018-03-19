fc194771f9c8b4c3547c0543907d547def486248
Unknown <vinc.claveau@gmail.com>
2018-03-19 17:54:40 +0100

    [IMP] mise en place des templates de malko
    footer et header
    + màj des traductions

---
+++ src/Controller/Visit/EditorController.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Virtual Visit application.
+ *
+ * Vincent Claveau <vinc.claveau@gmail.com>
+ *
+ */
+
+namespace App\Controller\Visit;
+
+use App\Entity\Visit;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/visit/editor", name="visit_editor_")
+ */
+class EditorController
+{
+    
+}
