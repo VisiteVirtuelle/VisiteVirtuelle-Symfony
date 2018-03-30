853e939d9ebc00852383d275175282cdf6391ed7
Unknown <vinc.claveau@gmail.com>
2018-03-30 14:47:15 +0200

    [ADD] form theme

--- src/Form/VisitType.php
+++ src/Form/VisitType.php
@@ -13,19 +13,42 @@ use App\Entity\Visit;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class VisitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('location')
-            ->add('sqft')
-            ->add('price')
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                ],
+            ])
+            ->add('location', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                ],
+            ])
+            ->add('sqft', IntegerType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                ],
+            ])
+            ->add('price', MoneyType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                ],
+            ])
             ->add('cover', FileType::class, [
                 'mapped' => false,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                ],
             ]);
     }
 
