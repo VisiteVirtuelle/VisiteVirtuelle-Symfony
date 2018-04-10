c380fd5f6b167dcc6fb677fb6a2f96b0b7927118
Unknown <valentin.pillon@lycee-bourdelle.fr>
2018-04-10 15:35:24 +0200

    [IMP] Amelioration bouton

--- public/visit.js
+++ public/visit.js
@@ -9,15 +9,17 @@ var isUserInteracting = false,
 
 //Lecture du XML de la visite
 var rooms = new Map();
+var roomsboutton = new Map();
 
 var INTERSECTED;
 const scene = new THREE.Scene();
 var mouse = new THREE.Vector2();
 var raycaster;
-var bottom = [];
+var boutton = [];
 raycaster = new THREE.Raycaster();
 var nombre;
 var clic;
+var testClicButton;
 
 getXHR();
 
@@ -30,7 +32,7 @@ const geometry = new THREE.SphereBufferGeometry(500, 60, 40);
 geometry.scale(-1, 1, 1);
 
 //console.log(rooms.values().next().value);
-affichageCube(bottom[0].test);
+if ( testClicButton === true) affichageCube(boutton[0].name);
 var texture = rooms.values().next().value;
 const material = new THREE.MeshBasicMaterial({map: texture,overdraw: 0.5});
 
@@ -122,7 +124,7 @@ function MouseWheel( event )
 function loadImg(path,test)
 {
     mesh.material.map = path;
-    affichageCube(test);
+    if ( testClicButton === true) affichageCube(test);
 }
 
 function getXHR()
@@ -141,50 +143,66 @@ function getXHR()
     xmlhttp.open("GET", "http://localhost:8000/visit/" + visit.id + "/visit.xml", false);
     xmlhttp.send();
     const xmlDoc = xmlhttp.responseXML;
-
+    
     var x = xmlDoc.getElementsByTagName("room");
     nombre = x.length;
-
+    
     for (var i = 0; i < x.length; i++)
-    {
+    {           
         rooms.set(
             x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue,
             new THREE.TextureLoader().load( "http://localhost:8000/visit/" + visit.id + "/" + x[i].getElementsByTagName("url")[0].childNodes[0].nodeValue)
         );
 
         var object = new THREE.Mesh( geometry2, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+        
+        if (x[i].getElementsByTagName("positionX")[0].childNodes[0].nodeValue === null || x[i].getElementsByTagName("positionY")[0].childNodes[0].nodeValue === null || x[i].getElementsByTagName("positionZ")[0].childNodes[0].nodeValue)
+        {
+            testClicButton = false;
+        }
+        else
+        {
+            testClicButton = true;
+            
+            object.position.x = x[i].getElementsByTagName("positionX")[0].childNodes[0].nodeValue;
+            object.position.y = x[i].getElementsByTagName("positionY")[0].childNodes[0].nodeValue;
+            object.position.z = x[i].getElementsByTagName("positionZ")[0].childNodes[0].nodeValue;
+        
 
-        object.position.x = x[i].getElementsByTagName("positionX")[0].childNodes[0].nodeValue;
-        object.position.y = x[i].getElementsByTagName("positionY")[0].childNodes[0].nodeValue;
-        object.position.z = x[i].getElementsByTagName("positionZ")[0].childNodes[0].nodeValue;
+            object.rotation.x = Math.random() * 2 * Math.PI;
+            object.rotation.y = Math.random() * 2 * Math.PI;
+            object.rotation.z = Math.random() * 2 * Math.PI;
 
-        object.rotation.x = Math.random() * 2 * Math.PI;
-        object.rotation.y = Math.random() * 2 * Math.PI;
-        object.rotation.z = Math.random() * 2 * Math.PI;
-
-        object.scale.x = Math.random() + 0.5;
-        object.scale.y = Math.random() + 0.5;
-        object.scale.z = Math.random() + 0.5;
-        object.path = new THREE.TextureLoader().load( "http://localhost:8000/visit/" + visit.id + "/" + x[i].getElementsByTagName("next")[0].childNodes[0].nodeValue);
-        object.test = x[i].getElementsByTagName("url")[0].childNodes[0].nodeValue;
-        object.testsuivant = x[i].getElementsByTagName("next")[0].childNodes[0].nodeValue;
-        //scene.add(object);
-        bottom.push(object);
+            object.scale.x = Math.random() + 0.5;
+            object.scale.y = Math.random() + 0.5;
+            object.scale.z = Math.random() + 0.5;
+           
+            object.path = x[i].getElementsByTagName("next")[0].childNodes[0].nodeValue;
+            object.name = x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+            //object.path = x[i].getElementsByTagName("next")[0].childNodes[0].nodeValue;
+            //alert(object.path);
+            
+            //scene.add(object);
+            boutton.push(object);
+        }
     }
 }
 
 function affichageCube(chemin)
 {
+    //alert("la salle est " + chemin);
     for (var i = 0; i < nombre; i ++)
     {
-        if (chemin === bottom[i].test)
+        if (chemin === boutton[i].name) 
         {
-            scene.add(bottom[i]);
+           // alert("ajout" + boutton[i].path );
+            scene.add(boutton[i]);
         }
         else
         {
-            scene.remove(bottom[i]);
-        }
+           // alert("supprimer" + boutton[i].path );
+            scene.remove(boutton[i]);
+        }           
     }
 }
 
@@ -223,54 +241,53 @@ function animate()
 {
     text2.style.top =  200 + 'px';
     text2.style.left =  (window.innerWidth/2 -40)  + 'px';
-
+    
     resizeCanvasToDisplaySize();
     requestAnimationFrame(animate);
     update();
     raycaste();
     renderer.render(scene, camera);
 }
-
+    
 
 function raycaste()
 {
-    raycaster.setFromCamera( mouse, camera );
-
-    var intersects = raycaster.intersectObjects( scene.children );
-
-    if(clic)
+    if (testClicButton)
     {
-        clic = false;
-        if(intersects.length > 0)
-        {
-            if(intersects[ 0 ].object)
-            {
-                if(INTERSECTED != intersects[ 0 ].object)
-                {
-                    if(INTERSECTED)
-                    {
-                        //INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-                    }
+        raycaster.setFromCamera( mouse, camera );
 
-                    if(intersects[ 0 ].object.material.emissive)
+        var intersects = raycaster.intersectObjects( scene.children );
+        
+        if(clic)
+        {
+            clic = false;
+            if(intersects.length > 0)
+            {
+                if(intersects[ 0 ].object) 
+                {
+                    if(INTERSECTED != intersects[ 0 ].object) 
                     {
-                        INTERSECTED = intersects[ 0 ].object;
-                        loadImg(INTERSECTED.path, INTERSECTED.testsuivant);
-                        //INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-                        //INTERSECTED.material.emissive.setHex( 0xffffff );
-                    }
-                    else
-                    {
-                        INTERSECTED = null;
+                        if(intersects[ 0 ].object.material.emissive)
+                        {
+                            INTERSECTED = intersects[ 0 ].object;
+                            //alert(INTERSECTED.path);
+                            //alert(rooms.get(INTERSECTED.path))
+                            loadImg(rooms.get(INTERSECTED.path), INTERSECTED.path);
+                        
+                        }
+                        else
+                        {
+                            INTERSECTED = null;
+                        } 
                     }
                 }
             }
+        } 
+        else 
+        {
+            if(INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+            INTERSECTED = null;  
         }
-    }
-    else
-    {
-        if(INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
-        INTERSECTED = null;
     }
 }
 
