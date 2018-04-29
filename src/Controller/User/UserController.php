66facd841be3a06a61ce0596dd37f427f2d207c5
Unknown <guillaume.vidal@lycee-bourdelle.fr>
2018-04-30 00:04:03 +0200

    [WIP] Dashboard - Sort User list

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -11,14 +11,16 @@ namespace App\Controller\User;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
  * @Route("/user_", name="user_")
  */
-class UserController
+class UserController extends Controller
 {
     private $userRepository;
 
@@ -26,16 +28,34 @@ class UserController
     {
         $this->userRepository = $userRepository;
     }
-
+    
     /**
      * @Route("/list", name="list")
      */
     public function list(Environment $twig)
     {
-        $users = $this->userRepository->findAll();
-
+        $column = '';
+        
+        $sortList = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->querySort($column);
+        
         return new Response($twig->render('User/list.html.twig', [
-            'users' => $users
+            'sortList' => $sortList
+        ]));
+    }
+    
+    /**
+     * @Route("/sortby/{column}", name="list_ordered")
+     */
+    public function listOrderedBy($column, Environment $twig)
+    {
+        $sortList = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->querySort($column);
+        
+        return new Response($twig->render('User/list.html.twig', [
+            'sortList' => $sortList
         ]));
     }
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -12,4 +12,44 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, User::class);
     }
+    
+    public function querySort($column): array
+    {
+        $entityManager = $this->getEntityManager();
+        
+        switch ($column)
+        {
+            case 'id':
+                $qb = $this->createQueryBuilder('list')
+                    ->orderBy('list.id', 'ASC')
+                    ->getQuery();
+                break;
+                
+            case 'fullname':
+                $qb = $this->createQueryBuilder('list')
+                    ->orderBy('list.fullName', 'ASC')
+                    ->getQuery();
+                break;
+                
+            case 'username':
+                $qb = $this->createQueryBuilder('list')
+                    ->orderBy('list.username', 'ASC')
+                    ->getQuery();
+                break;
+                
+            case 'email':
+                $qb = $this->createQueryBuilder('list')
+                    ->orderBy('list.email', 'ASC')
+                    ->getQuery();
+                break;
+                
+            default:
+                $qb = $this->createQueryBuilder('list')
+                    ->orderBy('list.id', 'ASC')
+                    ->getQuery();
+                break;
+        }
+        
+        return $qb->execute();
+    }
 }
