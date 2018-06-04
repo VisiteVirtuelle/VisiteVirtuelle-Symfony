c7cca4e8692e0bf4ff5152ecb86c3820b06f3c2e
Unknown <guillaume.vidal@lycee-bourdelle.fr>
2018-06-04 16:30:20 +0200

    [IMP] Test unitaire

--- tests/RegistrationControllerTest.php
+++ tests/RegistrationControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -9,24 +9,23 @@ class RegistrationControllerTest extends WebTestCase
     public function testCheckPassword()
     {
         $client = static::createClient();
-
+        
         $crawler = $client->request(
             'GET',
             '/en/register'
-        );
-
-        $form = $crawler->selectButton('Register')->form();
-
+            );
+        
+        $form = $crawler->selectButton('Submit')->form();
+        
         $form['user[email]'] = 'toto@email.com';
         $form['user[username]'] = 'usernametest';
-        $form['user[fullname]'] = 'John Doe';
         $form['user[password][first]'] = 'pass1';
         $form['user[password][second]'] = 'pass2';
-
+        
         $crawler = $client->submit($form);
-
+        
         $this->assertEquals(1,
             $crawler->filter('li:contains("This value is not valid.")')->count()
-        );
+            );
     }
 }
